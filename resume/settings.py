23a67037c3f1599b2f59e501a0bed8bf478d254a
kush9871 <108216371+kush9871@users.noreply.github.com>
2022-09-18 00:35:59 +0530

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 
 #//////////// custom settings///////////////
 
-STAIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = 'static/'
-# STATIC_ROOT = "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
